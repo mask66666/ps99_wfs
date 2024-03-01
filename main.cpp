25c4629bec2b20aec4f542c62d2998099c0fdf15
1mask_1 <damianwal95@gmail.com>
2024-03-02 00:23:04 +0100

    finished?

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 #include <chrono>
 #include <fstream>
 
+
+#define WHITE { 255, 255, 255 }
+
+#define BAR_X 1729 // normal: 1491 left half: 1729
+#define BAR_Y 239 // normal: 237 left half: 239
+
+#define TOL 30
+
+
+
 // gets color of specific pixel and returns RGB
 std::array<int, 3> get_rgb(int x, int y) {
     HWND hwndD = GetDesktopWindow();
@@ -48,11 +58,45 @@ void sleep(int duration) {
 }
 
 
-int main() {
-    std::array<int, 3> color = get_rgb(100, 200);
-    std::array<int, 3> color2 = get_rgb(100, 100);
 
-    bool res = compeare_color(color, color2, 20);
-    std::cout << res << std::endl;
+
+
+
+void fish() {
+    sleep(750);
+    click();
+    int x, y;
+    get_pos(x, y);
+    std::array color = get_rgb(x, y);
+    bool res = compeare_color(color, WHITE, TOL);
+    while (!res) {
+        get_pos(x, y);
+        color = get_rgb(x, y);
+        res = compeare_color(color, WHITE, TOL);
+    }
+    sleep(100);
+    click();
+}
+
+void click_to_fill_bar() {
+    sleep(200);
+    std::array color = get_rgb(BAR_X, BAR_Y);
+    bool res = compeare_color(color, WHITE, TOL);
+    while (res) {
+        color = get_rgb(BAR_X, BAR_Y);
+        res = compeare_color(color, WHITE, TOL);
+        click();
+        sleep(50);
+    }
+}
+
+
+int main() {
     
+
+    fish();
+
+    sleep(500);
+
+    click_to_fill_bar();
 }
