61059d8b70d4da2156c6e332cc465b83fde82492
1mask_1 <damianwal95@gmail.com>
2024-03-01 23:45:38 +0100

    started adding get_rgb

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <windows.h>
 #include <array>
 
+std::array<int, 3> get_rgb(int x, int y) {
+    HWND hwndD = GetDesktopWindow();
+    HDC hdcD = GetDC(hwndD);
+
+    COLORREF color = GetPixel(hdcD, x, y);
+}
+
+
 int main() {
     std::cout << "Hello Wrold" << std::endl; // testing and setup
 }
